3371bee4328a567535a4f936e1fd72f2bc1769cf
ryanl <leryan@msu.edu>
2022-02-24 18:14:23 -0500

    Check

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author ryanl
+ */
+
+#include "pch.h"
+#include "SpartyGnomeApp.h"
+
+/**
+* Executes AquariumApp as a wxWidgets app
+*/
+wxIMPLEMENT_APP(SpartyGnomeApp);
+
