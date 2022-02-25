336c85cc9663a500af873a679486f8ad1da74181
Ashrey Gupta <guptaas8@msu.edu>
2022-02-25 17:01:58 -0500

    Class ItemSpartyGnome created by Ashrey Gupta

---
+++ SpartyGnomeLib/ItemSpartyGnome.h
@@ -0,0 +1,20 @@
+/**
+ * @file ItemSpartyGnome.h
+ * @author ashre
+ *
+ *
+ */
+
+#ifndef SPARTYGNOME_ITEMSPARTYGNOME_H
+#define SPARTYGNOME_ITEMSPARTYGNOME_H
+
+#include "Item.h"
+
+class ItemSpartyGnome:public Item {
+private:
+
+public:
+
+};
+
+#endif //SPARTYGNOME_ITEMSPARTYGNOME_H
