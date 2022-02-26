19bfdea99a23216887c24686d1cb58cba1b122ef
SSithub <57548627+ssitu@users.noreply.github.com>
2022-02-26 18:41:49 -0500

    Start of the Villain class

---
+++ SpartyGnomeLib/Villain.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file Villain.cpp
+ * 
+ * @author Simon Situ
+ */
+
+#include "Villain.h"
+
+Villain::Villain(Game* game, const std::wstring& imagePath) : Item(game, imagePath)
+{
+
+}
---
+++ SpartyGnomeLib/Villain.h
@@ -0,0 +1,30 @@
+/**
+ * @file Villain.h
+ * 
+ * @author Simon Situ
+ *
+ * The villain class
+ */
+
+#ifndef SPARTYGNOME_VILLAIN_H
+#define SPARTYGNOME_VILLAIN_H
+
+#include "Item.h"
+
+/**
+ * The villain class
+ */
+class Villain : public Item {
+private:
+
+public:
+    /// Disable defaults
+    Villain() = delete;
+    Villain(const Villain&) = delete;
+    void operator=(const Villain&) = delete;
+
+    Villain(Game* game, const std::wstring& imagePath);
+
+};
+
+#endif //SPARTYGNOME_VILLAIN_H
