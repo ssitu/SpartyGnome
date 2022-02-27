3c52e16cb9c8a6d3da32362216da4b2f79539e60
SSithub <57548627+ssitu@users.noreply.github.com>
2022-02-26 22:53:15 -0500

    Villain constructor documentation

--- SpartyGnomeLib/Villain.cpp
+++ SpartyGnomeLib/Villain.cpp
@@ -6,6 +6,11 @@
 
 #include "Villain.h"
 
+/**
+ * Villain Constructor
+ * @param game The game holding this item
+ * @param imagePath The path to the image for this item
+ */
 Villain::Villain(Game* game, const std::wstring& imagePath) : Item(game, imagePath)
 {
 
