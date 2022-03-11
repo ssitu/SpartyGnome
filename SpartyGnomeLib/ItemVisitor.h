b08fd78b196c11b090dd96893192636ce55e3668
Simon Situ <situsimo@msu.edu>
2022-03-10 19:58:16 -0500

    Started on the visitor class needed for separating drawing between scrolling items and non-scrolling items

---
+++ SpartyGnomeLib/ItemVisitor.h
@@ -0,0 +1,58 @@
+/**
+ * @file ItemVisitor.h
+ * 
+ * @author Simon Situ
+ *
+ * The visitor class for item objects
+ */
+
+#ifndef SPARTYGNOME_ITEMVISITOR_H
+#define SPARTYGNOME_ITEMVISITOR_H
+
+// Forward references to all item types
+class ItemSpartyGnome;
+class Platform;
+class Wall;
+class Villain;
+class ItemDoor;
+
+/**
+ * The visitor class for item objects
+ */
+class ItemVisitor {
+private:
+
+public:
+    /**
+     * Visit a Gnome
+     * @param gnome The Gnome to visit
+     */
+    virtual void VisitGnome(ItemSpartyGnome* gnome) {}
+
+    /**
+     * Visit a Platform
+     * @param platform The Platform to visit
+     */
+    virtual void VisitPlatform(Platform* platform) {}
+
+    /**
+     * Visit a Wall
+     * @param wall The Wall to visit
+     */
+    virtual void VisitWall(Wall* wall) {}
+
+    /**
+     * Visit a Villain
+     * @param villain The Villain to visit
+     */
+    virtual void VisitVillain(Villain* villain) {}
+
+    /**
+     * Visit a Door
+     * @param door The Door to visit
+     */
+    virtual void VisitDoor(ItemDoor* door) {}
+
+};
+
+#endif //SPARTYGNOME_ITEMVISITOR_H
